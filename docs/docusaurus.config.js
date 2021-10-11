2f368561d98a6a6643df9c3c9a554a3893c23514
Paulo Ragonha <paulo@ragonha.me>
2021-10-11 21:37:45 +0200

    Attempting to deploy (https://docusaurus.io/docs/deployment#triggering-deployment-with-github-actions)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,13 +8,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+
+  url: "https://glowing-pancake-951ea524.pages.github.io/",
+  baseUrl: "/",
+  projectName: "ore-ui",
+  organizationName: "Mojang",
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   presets: [
     [
