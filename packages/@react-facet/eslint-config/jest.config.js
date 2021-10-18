caa3bee9c8fb984f6c68b3e94b96afb40349ba00
Paulo Ragonha <paulo@ragonha.me>
2021-10-18 13:35:09 +0200

    Final tweaks before attempting to publish packages to NPM registry

--- packages/@react-facet/eslint-config/jest.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('../../../jest.base.config')
-const projectName = require('./package.json').name
-
-module.exports = {
-  ...baseConfig,
-  name: projectName,
-  displayName: projectName,
-  rootDir: './',
-}
