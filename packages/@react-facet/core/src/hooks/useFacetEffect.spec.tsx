d4518799aa3ca4c0be27ff6b40b72909f74d49dd
Paulo Ragonha <paulo@ragonha.me>
2021-12-08 10:07:02 +0100

    Allow useFacetEffect to accept multiple facets

--- packages/@react-facet/core/src/hooks/useFacetEffect.spec.tsx
+++ packages/@react-facet/core/src/hooks/useFacetEffect.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { act, render } from '@react-facet/dom-fiber-testing-library'
 import { useFacetEffect } from './useFacetEffect'
 import { createFacet } from '../facet'
+import { NO_VALUE } from '../types'
 
 it('triggers the effect on mount with the initial value and on any update of the facet', () => {
   const demoFacet = createFacet({ initialValue: 'initial value' })
@@ -14,7 +15,7 @@ it('triggers the effect on mount with the initial value and on any update of the
         callback(value)
       },
       [],
-      demoFacet,
+      [demoFacet],
     )
 
     return null
@@ -49,7 +50,7 @@ it('triggers the effect when a dependency changes', () => {
         callback(`${value} ${dependency}`)
       },
       [dependency],
-      demoFacet,
+      [demoFacet],
     )
 
     return null
@@ -81,7 +82,7 @@ describe('cleanup', () => {
           return cleanup
         },
         [],
-        demoFacet,
+        [demoFacet],
       )
 
       return null
@@ -110,4 +111,75 @@ describe('cleanup', () => {
     // once a new value is triggered we expect that the previous cleanup was called
     expect(cleanup).toHaveBeenCalled()
   })
+})
+
+it('supports multiple facets, only triggering the effect once all facets have a value', () => {
+  const facetA = createFacet<string>({ initialValue: 'initial value' })
+  const facetB = createFacet<number>({ initialValue: NO_VALUE })
+
+  const cleanup = jest.fn()
+  const effect = jest.fn().mockReturnValue(cleanup)
+
+  const ComponentWithFacetEffect = () => {
+    useFacetEffect(
+      (valueA, valueB) => {
+        return effect(valueA, valueB)
+      },
+      [],
+      [facetA, facetB],
+    )
+
+    return null
+  }
+
+  const scenario = <ComponentWithFacetEffect />
+
+  const { rerender } = render(scenario)
+
+  expect(cleanup).not.toHaveBeenCalled()
+  expect(effect).not.toHaveBeenCalled()
+  effect.mockClear()
+  cleanup.mockClear()
+
+  // change only the initialized facet
+  act(() => {
+    facetA.set('new value')
+  })
+
+  // expect that the effect was not called
+  expect(cleanup).not.toHaveBeenCalled()
+  expect(effect).not.toHaveBeenCalled()
+  effect.mockClear()
+  cleanup.mockClear()
+
+  // set a value for the second facet
+  act(() => {
+    facetB.set(123)
+  })
+
+  // verify that the effect was finally called with both values
+  expect(cleanup).not.toHaveBeenCalled()
+  expect(effect).toHaveBeenCalledWith('new value', 123)
+  expect(effect).toHaveBeenCalledTimes(1)
+  effect.mockClear()
+  cleanup.mockClear()
+
+  // update the first facet again
+  act(() => {
+    facetA.set('one more update')
+  })
+
+  // verify that the effect was called again, and now we verify that the previous cleanup was called
+  expect(cleanup).toHaveBeenCalledTimes(1)
+  expect(effect).toHaveBeenCalledWith('one more update', 123)
+  expect(effect).toHaveBeenCalledTimes(1)
+  effect.mockClear()
+  cleanup.mockClear()
+
+  // and finally we unmount the component
+  rerender(<></>)
+
+  // then we get a final cleanup, without the effect being fired
+  expect(cleanup).toHaveBeenCalledTimes(1)
+  expect(effect).not.toHaveBeenCalled()
 })
