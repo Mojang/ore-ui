f245fbb6333943da4ab7ec0d0b25b9c813776b0f
Fernando Via Canel <fernando.via@gmail.com>
2022-01-27 15:47:51 +0100

    Add With component

---
+++ packages/@react-facet/core/src/components/With.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+import { useFacetMap, useFacetUnwrap } from '../hooks'
+import { Facet } from '../types'
+
+type WithProps<T> = {
+  data: Facet<T | undefined>
+  children: (data: Facet<T>) => ReactElement
+}
+
+export const With = <T,>({ data, children }: WithProps<T>) => {
+  const shouldRenderFacet = useFacetMap((data) => data != null, [], [data])
+  const shouldRender = useFacetUnwrap(shouldRenderFacet)
+  return shouldRender === true ? children(data as Facet<T>) : null
+}
