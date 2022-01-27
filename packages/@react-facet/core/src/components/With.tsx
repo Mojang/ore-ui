544fdf14f687927be897dc5fed52935efe8994c3
Fernando Via Canel <fernando.via@gmail.com>
2022-01-27 16:25:29 +0100

    Update the With component with type guard instead of casting
    Co-authored-by: Danila Dergachev <danila.dergachev@mojang.com>

--- packages/@react-facet/core/src/components/With.tsx
+++ packages/@react-facet/core/src/components/With.tsx
@@ -7,8 +7,12 @@ type WithProps<T> = {
   children: (data: Facet<T>) => ReactElement
 }
 
+const hasData = <T,>(_: Facet<T | null | undefined>, shouldRender: boolean | NoValue): _ is Facet<T> => {
+  return shouldRender === true
+}
+
 export const With = <T,>({ data, children }: WithProps<T>) => {
   const shouldRenderFacet = useFacetMap((data) => data != null, [], [data])
   const shouldRender = useFacetUnwrap(shouldRenderFacet)
-  return shouldRender === true ? children(data as Facet<T>) : null
+  return hasData(data, shouldRender) ? children(data) : null
 }
