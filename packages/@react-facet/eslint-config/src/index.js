5de75b8823c26f77df2eeac49ec8b202c84d7441
Jacob Bergdahl <jacob_bergdahl@live.se>
2022-01-28 09:51:44 +0100

    Rename useFacetEffect to useFacetLayoutEffect and add a new useFacetEffect (#30)
    * Rename useFacetEffect to useFacetLayoutEffect
    
    * Add a useFacetEffect that is based on useEffect
    
    * Add the newly added useFacetEffect to index files
    
    * Use the useFacetEffect function in the code implementations instead of useFacetLayoutEffect
    
    * Update the function comments for useFacetEffect and useFacetLayoutEffect
    
    Co-authored-by: Jacob Bergdahl <v-bbergdahl@microsoft.com>

--- packages/@react-facet/eslint-config/src/index.js
+++ packages/@react-facet/eslint-config/src/index.js
@@ -6,7 +6,7 @@ module.exports = {
     'react-hooks/exhaustive-deps': [
       'error',
       {
-        additionalHooks: '(useFacetEffect|useFacetMap|useFacetMemo|useFacetCallback)',
+        additionalHooks: '(useFacetLayoutEffect|useFacetEffect|useFacetMap|useFacetMemo|useFacetCallback)',
       },
     ],
   },
