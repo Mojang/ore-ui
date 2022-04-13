d6e1d8524f946b3a7652befbafe9fe74f106e50f
Viktor Bergehall <viktor@viktorbergehall.me>
2022-04-13 08:55:59 +0200

    Refactored implementation into new component

--- packages/@react-facet/core/src/components/Map.spec.tsx
+++ packages/@react-facet/core/src/components/Map.spec.tsx
@@ -3,299 +3,444 @@ import { act, render } from '@react-facet/dom-fiber-testing-library'
 import { createFacet } from '../facet'
 import { Facet, NoValue } from '../types'
 import { useFacetEffect, useFacetMap } from '../hooks'
-import { Map } from '.'
+import { Map, MapWithKey } from '.'
 
-it('renders all items in a Facet of array', () => {
-  type Input = { key: string; data: string }
-  const data = createFacet<Input[]>({
-    initialValue: [
-      { key: '1', data: 'a' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-    ],
+describe('<Map />', () => {
+  it('renders all items in a Facet of array', () => {
+    type Input = { a: string }
+    const data = createFacet({
+      initialValue: [{ a: '1' }, { a: '2' }, { a: '3' }, { a: '4' }, { a: '5' }],
+    })
+
+    const ExampleContent = ({ item, index }: { item: Facet<Input>; index: number }) => {
+      return (
+        <span>
+          <fast-text text={useFacetMap(({ a }) => a, [], [item])} />
+          <span>{index}</span>
+        </span>
+      )
+    }
+
+    const inputEqualityCheck = () => {
+      const previous: Partial<Input> = {}
+
+      return (current: Input) => {
+        if (current.a === previous.a) {
+          return true
+        }
+        previous.a = current.a
+        return false
+      }
+    }
+    const Example = () => {
+      return (
+        <Map array={data} equalityCheck={inputEqualityCheck}>
+          {(item, index) => <ExampleContent item={item} index={index} />}
+        </Map>
+      )
+    }
+
+    const scenario = <Example />
+
+    const { container } = render(scenario)
+
+    expect(container).toMatchSnapshot()
   })
 
-  const ExampleContent = ({ item, index }: { item: Facet<Input>; index: number }) => {
-    return (
+  it('unmounts components when the array reduces in size', () => {
+    interface Item {
+      value: string
+    }
+
+    const data = createFacet<Item[]>({
+      initialValue: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
+    })
+
+    const Item = ({ item, index }: { item: Facet<Item>; index: number }) => (
+      <span>
+        <fast-text text={useFacetMap(({ value }) => value, [], [item])} />
+        <span>{index}</span>
+      </span>
+    )
+
+    const itemEqualityCheck = () => {
+      const previous: Partial<Item> = {}
+
+      return (current: Item) => {
+        if (current.value === previous.value) {
+          return true
+        }
+
+        previous.value = current.value
+
+        return false
+      }
+    }
+
+    const Example = () => {
+      return (
+        <Map array={data} equalityCheck={itemEqualityCheck}>
+          {(item, index) => <Item item={item} index={index} />}
+        </Map>
+      )
+    }
+
+    const scenario = <Example />
+
+    const { container } = render(scenario)
+
+    expect(container).toMatchSnapshot()
+
+    data.set([{ value: '1' }, { value: '2' }])
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('updates only items that have changed', () => {
+    type Input = { a: string }
+    const data = createFacet({
+      initialValue: [{ a: '1' }, { a: '2' }, { a: '3' }, { a: '4' }, { a: '5' }],
+    })
+
+    const mock = jest.fn()
+
+    const ExampleContent = ({ item }: { item: Facet<Input> }) => {
+      useFacetEffect(mock, [], [item])
+      return null
+    }
+
+    const inputEqualityCheck = () => {
+      const previous: Partial<Input> = {}
+
+      return (current: Input) => {
+        if (current.a === previous.a) {
+          return true
+        }
+
+        previous.a = current.a
+
+        return false
+      }
+    }
+
+    const Example = () => {
+      return (
+        <Map array={data} equalityCheck={inputEqualityCheck}>
+          {(item) => <ExampleContent item={item} />}
+        </Map>
+      )
+    }
+
+    const scenario = <Example />
+
+    render(scenario)
+
+    expect(mock).toHaveBeenCalledTimes(5)
+
+    mock.mockClear()
+
+    act(() => {
+      data.set([{ a: '6' }, { a: '2' }, { a: '3' }, { a: '4' }, { a: '5' }])
+    })
+
+    expect(mock).toHaveBeenCalledTimes(1)
+    expect(mock).toHaveBeenCalledWith({ a: '6' })
+  })
+})
+
+describe('<MapWithKey />', () => {
+  it('renders all items in a Facet of array', () => {
+    type Input = { key: string; data: string }
+    const data = createFacet<Input[]>({
+      initialValue: [
+        { key: '1', data: 'a' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ],
+    })
+
+    const ExampleContent = ({ item, index }: { item: Facet<Input>; index: number }) => {
+      return (
+        <span>
+          <fast-text text={useFacetMap(({ data }) => data, [], [item])} />
+          <span>{index}</span>
+        </span>
+      )
+    }
+
+    const keySelector = ({ key }: Input) => key
+
+    const inputEqualityCheck = () => {
+      const previous: Partial<Input> = {}
+
+      return (current: Input) => {
+        if (current.key === previous.key && current.data === previous.data) {
+          return true
+        }
+
+        previous.key = current.key
+        previous.data = current.data
+
+        return false
+      }
+    }
+
+    const Example = () => {
+      return (
+        <MapWithKey array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
+          {(item, index) => <ExampleContent item={item} index={index} />}
+        </MapWithKey>
+      )
+    }
+
+    const scenario = <Example />
+
+    const { container } = render(scenario)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('unmounts components when the array reduces in size', () => {
+    type Input = { key: string; data: string }
+    const data = createFacet<Input[]>({
+      initialValue: [
+        { key: '1', data: 'a' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ],
+    })
+
+    const Item = ({ item, index }: { item: Facet<Input>; index: number }) => (
       <span>
         <fast-text text={useFacetMap(({ data }) => data, [], [item])} />
         <span>{index}</span>
       </span>
     )
-  }
 
-  const keySelector = ({ key }: Input) => key
+    const keySelector = ({ key }: Input) => key
 
-  const inputEqualityCheck = () => {
-    const previous: Partial<Input> = {}
+    const itemEqualityCheck = () => {
+      const previous: Partial<Input> = {}
 
-    return (current: Input) => {
-      if (current.key === previous.key && current.data === previous.data) {
-        return true
+      return (current: Input) => {
+        if (current.key === previous.key && current.data === previous.data) {
+          return true
+        }
+
+        previous.key = current.key
+        previous.data = current.data
+
+        return false
       }
-
-      previous.key = current.key
-      previous.data = current.data
-
-      return false
     }
-  }
 
-  const Example = () => {
-    return (
-      <Map array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
-        {(item, index) => <ExampleContent item={item} index={index} />}
-      </Map>
-    )
-  }
+    const Example = () => {
+      return (
+        <MapWithKey array={data} equalityCheck={itemEqualityCheck} keySelector={keySelector}>
+          {(item, index) => <Item item={item} index={index} />}
+        </MapWithKey>
+      )
+    }
 
-  const scenario = <Example />
+    const scenario = <Example />
 
-  const { container } = render(scenario)
+    const { container } = render(scenario)
 
-  expect(container).toMatchSnapshot()
-})
+    expect(container).toMatchSnapshot()
 
-it('unmounts components when the array reduces in size', () => {
-  type Input = { key: string; data: string }
-  const data = createFacet<Input[]>({
-    initialValue: [
+    data.set([
       { key: '1', data: 'a' },
       { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-    ],
-  })
-
-  const Item = ({ item, index }: { item: Facet<Input>; index: number }) => (
-    <span>
-      <fast-text text={useFacetMap(({ data }) => data, [], [item])} />
-      <span>{index}</span>
-    </span>
-  )
-
-  const keySelector = ({ key }: Input) => key
-
-  const itemEqualityCheck = () => {
-    const previous: Partial<Input> = {}
-
-    return (current: Input) => {
-      if (current.key === previous.key && current.data === previous.data) {
-        return true
-      }
-
-      previous.key = current.key
-      previous.data = current.data
-
-      return false
-    }
-  }
-
-  const Example = () => {
-    return (
-      <Map array={data} equalityCheck={itemEqualityCheck} keySelector={keySelector}>
-        {(item, index) => <Item item={item} index={index} />}
-      </Map>
-    )
-  }
-
-  const scenario = <Example />
-
-  const { container } = render(scenario)
-
-  expect(container).toMatchSnapshot()
-
-  data.set([
-    { key: '1', data: 'a' },
-    { key: '2', data: 'b' },
-  ])
-
-  expect(container).toMatchSnapshot()
-})
-
-it('updates only items that have changed', () => {
-  type Input = { key: string; data: string }
-  const data = createFacet<Input[]>({
-    initialValue: [
-      { key: '1', data: 'a' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-    ],
-  })
-
-  const mock = jest.fn()
-
-  const ExampleContent = ({ item }: { item: Facet<Input> }) => {
-    useFacetEffect(mock, [], [item])
-    return null
-  }
-
-  const keySelector = ({ key }: Input) => key
-
-  const inputEqualityCheck = () => {
-    const previous: Partial<Input> = {}
-
-    return (current: Input) => {
-      if (current.key === previous.key && current.data === previous.data) {
-        return true
-      }
-
-      previous.key = current.key
-      previous.data = current.data
-
-      return false
-    }
-  }
-
-  const Example = () => {
-    return (
-      <Map array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
-        {(item) => <ExampleContent item={item} />}
-      </Map>
-    )
-  }
-
-  const scenario = <Example />
-
-  render(scenario)
-
-  expect(mock).toHaveBeenCalledTimes(5)
-
-  mock.mockClear()
-
-  act(() => {
-    data.set([
-      { key: '1', data: 'z' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
     ])
+
+    expect(container).toMatchSnapshot()
   })
 
-  expect(mock).toHaveBeenCalledTimes(1)
-  expect(mock).toHaveBeenCalledWith({ key: '1', data: 'z' })
-})
+  it('updates only items that have changed', () => {
+    type Input = { key: string; data: string }
+    const data = createFacet<Input[]>({
+      initialValue: [
+        { key: '1', data: 'a' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ],
+    })
 
-it('rerenders only when key is updated or added', () => {
-  type Input = { key: string; data: string }
-  const data = createFacet<Input[]>({
-    initialValue: [
-      { key: '1', data: 'a' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-    ],
-  })
+    const mock = jest.fn()
 
-  const facetUpdateMock = jest.fn()
-  const onMount = jest.fn()
-
-  const ExampleContent = ({
-    item,
-    index,
-  }: {
-    index: number
-    item: Facet<Input>
-    onMount: (input: Input | NoValue) => void
-  }) => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => onMount(index), [])
-    useFacetEffect((itemValue) => facetUpdateMock(itemValue, index), [index], [item])
-    const dataFacet = useFacetMap(({ data }) => `Data: ${data}`, [], [item])
-    const keyFacet = useFacetMap(({ key }) => `Key: ${key}`, [], [item])
-
-    return (
-      <div>
-        <p>
-          <fast-text text={keyFacet} />
-        </p>
-        <p>
-          <fast-text text={dataFacet} />
-        </p>
-      </div>
-    )
-  }
-
-  const keySelector = ({ key }: Input) => key
-
-  const inputEqualityCheck = () => {
-    const previous: Partial<Input> = {}
-
-    return (current: Input) => {
-      if (current.key === previous.key && current.data === previous.data) {
-        return true
-      }
-
-      previous.key = current.key
-      previous.data = current.data
-
-      return false
+    const ExampleContent = ({ item }: { item: Facet<Input> }) => {
+      useFacetEffect(mock, [], [item])
+      return null
     }
-  }
 
-  const Example = () => {
-    return (
-      <Map array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
-        {(item, index) => <ExampleContent item={item} index={index} onMount={facetUpdateMock} />}
-      </Map>
-    )
-  }
+    const keySelector = ({ key }: Input) => key
 
-  const scenario = <Example />
+    const inputEqualityCheck = () => {
+      const previous: Partial<Input> = {}
 
-  const { container } = render(scenario)
+      return (current: Input) => {
+        if (current.key === previous.key && current.data === previous.data) {
+          return true
+        }
 
-  expect(facetUpdateMock).toHaveBeenCalledTimes(5)
-  expect(onMount).toHaveBeenCalledTimes(5)
+        previous.key = current.key
+        previous.data = current.data
 
-  expect(container).toMatchSnapshot()
+        return false
+      }
+    }
 
-  facetUpdateMock.mockClear()
-  onMount.mockClear()
+    const Example = () => {
+      return (
+        <MapWithKey array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
+          {(item) => <ExampleContent item={item} />}
+        </MapWithKey>
+      )
+    }
 
-  act(() => {
-    data.set([
-      { key: '1', data: 'z' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-    ])
+    const scenario = <Example />
+
+    render(scenario)
+
+    expect(mock).toHaveBeenCalledTimes(5)
+
+    mock.mockClear()
+
+    act(() => {
+      data.set([
+        { key: '1', data: 'z' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ])
+    })
+
+    expect(mock).toHaveBeenCalledTimes(1)
+    expect(mock).toHaveBeenCalledWith({ key: '1', data: 'z' })
   })
 
-  expect(facetUpdateMock).toHaveBeenCalledTimes(1)
-  expect(facetUpdateMock).toHaveBeenCalledWith({ key: '1', data: 'z' }, 0)
-  expect(onMount).not.toHaveBeenCalled()
+  it('rerenders only when key is updated or added', () => {
+    type Input = { key: string; data: string }
+    const data = createFacet<Input[]>({
+      initialValue: [
+        { key: '1', data: 'a' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ],
+    })
 
-  expect(container).toMatchSnapshot()
+    const facetUpdateMock = jest.fn()
+    const onMount = jest.fn()
 
-  facetUpdateMock.mockClear()
-  onMount.mockClear()
+    const ExampleContent = ({
+      item,
+      index,
+    }: {
+      index: number
+      item: Facet<Input>
+      onMount: (input: Input | NoValue) => void
+    }) => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      useEffect(() => onMount(index), [])
+      useFacetEffect((itemValue) => facetUpdateMock(itemValue, index), [index], [item])
+      const dataFacet = useFacetMap(({ data }) => `Data: ${data}`, [], [item])
+      const keyFacet = useFacetMap(({ key }) => `Key: ${key}`, [], [item])
 
-  act(() => {
-    data.set([
-      { key: '1', data: 'z' },
-      { key: '2', data: 'b' },
-      { key: '3', data: 'c' },
-      { key: '4', data: 'd' },
-      { key: '5', data: 'e' },
-      { key: '6', data: 'f' },
-    ])
+      return (
+        <div>
+          <p>
+            <fast-text text={keyFacet} />
+          </p>
+          <p>
+            <fast-text text={dataFacet} />
+          </p>
+        </div>
+      )
+    }
+
+    const keySelector = ({ key }: Input) => key
+
+    const inputEqualityCheck = () => {
+      const previous: Partial<Input> = {}
+
+      return (current: Input) => {
+        if (current.key === previous.key && current.data === previous.data) {
+          return true
+        }
+
+        previous.key = current.key
+        previous.data = current.data
+
+        return false
+      }
+    }
+
+    const Example = () => {
+      return (
+        <MapWithKey array={data} equalityCheck={inputEqualityCheck} keySelector={keySelector}>
+          {(item, index) => <ExampleContent item={item} index={index} onMount={facetUpdateMock} />}
+        </MapWithKey>
+      )
+    }
+
+    const scenario = <Example />
+
+    const { container } = render(scenario)
+
+    expect(facetUpdateMock).toHaveBeenCalledTimes(5)
+    expect(onMount).toHaveBeenCalledTimes(5)
+
+    expect(container).toMatchSnapshot()
+
+    facetUpdateMock.mockClear()
+    onMount.mockClear()
+
+    act(() => {
+      data.set([
+        { key: '1', data: 'z' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+      ])
+    })
+
+    expect(facetUpdateMock).toHaveBeenCalledTimes(1)
+    expect(facetUpdateMock).toHaveBeenCalledWith({ key: '1', data: 'z' }, 0)
+    expect(onMount).not.toHaveBeenCalled()
+
+    expect(container).toMatchSnapshot()
+
+    facetUpdateMock.mockClear()
+    onMount.mockClear()
+
+    act(() => {
+      data.set([
+        { key: '1', data: 'z' },
+        { key: '2', data: 'b' },
+        { key: '3', data: 'c' },
+        { key: '4', data: 'd' },
+        { key: '5', data: 'e' },
+        { key: '6', data: 'f' },
+      ])
+    })
+
+    expect(facetUpdateMock).toHaveBeenCalledTimes(1)
+    expect(facetUpdateMock).toHaveBeenCalledWith({ key: '6', data: 'f' }, 5)
+    expect(onMount).toHaveBeenCalledWith(5)
+
+    expect(container).toMatchSnapshot()
+
+    facetUpdateMock.mockClear()
+    onMount.mockClear()
   })
-
-  expect(facetUpdateMock).toHaveBeenCalledTimes(1)
-  expect(facetUpdateMock).toHaveBeenCalledWith({ key: '6', data: 'f' }, 5)
-  expect(onMount).toHaveBeenCalledWith(5)
-
-  expect(container).toMatchSnapshot()
-
-  facetUpdateMock.mockClear()
-  onMount.mockClear()
 })
--- packages/@react-facet/core/src/components/Map.tsx
+++ packages/@react-facet/core/src/components/Map.tsx
@@ -1,15 +1,93 @@
 import React, { ReactElement } from 'react'
-import { useFacetMap, useFacetUnwrap, useFacetMemo } from '../hooks'
+import { useFacetMap, useFacetMemo, useFacetUnwrap } from '../hooks'
 import { EqualityCheck, Facet, NO_VALUE } from '../types'
 
 export type MapProps<T> = {
-  children: (item: Facet<T>, index: number) => ReactElement
   array: Facet<T[]>
-  keySelector?: (item: T) => string
+  children: (item: Facet<T>, index: number) => ReactElement
   equalityCheck?: EqualityCheck<T>
 }
 
-export const Map = <T,>({ array, children, equalityCheck, keySelector }: MapProps<T>) => {
+export const Map = <T,>({ array, children, equalityCheck }: MapProps<T>) => {
+  const countValue = useFacetUnwrap(useFacetMap((array) => array.length, [], [array])) ?? 0
+
+  return (
+    <>
+      {times(
+        (index) =>
+          equalityCheck != null ? (
+            <MapChildMemo<T>
+              key={index}
+              arrayFacet={array}
+              index={index}
+              equalityCheck={equalityCheck}
+              children={children}
+            />
+          ) : (
+            <MapChild<T> key={index} arrayFacet={array} index={index} children={children} />
+          ),
+        countValue !== NO_VALUE ? countValue : 0,
+      )}
+    </>
+  )
+}
+
+type MapChildMemoProps<T> = {
+  arrayFacet: Facet<T[]>
+  index: number
+  children: (item: Facet<T>, index: number) => ReactElement
+  equalityCheck: EqualityCheck<T>
+}
+
+const MapChildMemo = <T,>({ arrayFacet, index, children, equalityCheck }: MapChildMemoProps<T>) => {
+  const childFacet = useFacetMemo(
+    (array) => {
+      if (index < array.length) return array[index]
+      return NO_VALUE
+    },
+    [index],
+    [arrayFacet],
+    equalityCheck,
+  )
+  return children(childFacet, index)
+}
+
+type MapChildProps<T> = {
+  arrayFacet: Facet<T[]>
+  index: number
+  children: (item: Facet<T>, index: number) => ReactElement
+}
+
+const MapChild = <T,>({ arrayFacet, index, children }: MapChildProps<T>) => {
+  const childFacet = useFacetMap(
+    (array) => {
+      if (index < array.length) return array[index]
+      return NO_VALUE
+    },
+    [index],
+    [arrayFacet],
+  )
+
+  return children(childFacet, index)
+}
+
+interface TimesFn<T> {
+  (index: number): T
+}
+
+const times = <T,>(fn: TimesFn<T>, n: number) => {
+  const result = []
+
+  for (let index = 0; index < n; index++) {
+    result.push(fn(index))
+  }
+
+  return result
+}
+
+export type MapWithKeyProps<T> = MapProps<T> & { keySelector?: (item: T) => React.Key }
+
+export const MapWithKey = <T,>({ array, children, equalityCheck, keySelector }: MapWithKeyProps<T>) => {
   const arrayLength = useFacetUnwrap(useFacetMap((array) => array.length, [], [array]))
   const arrayUnwrapped = array.get()
 
@@ -21,7 +99,7 @@ export const Map = <T,>({ array, children, equalityCheck, keySelector }: MapProp
         const key = keySelector?.(item) ?? index
 
         return (
-          <Child<T>
+          <MapWithKeyChild<T>
             arrayFacet={array}
             key={key}
             index={index}
@@ -34,8 +112,7 @@ export const Map = <T,>({ array, children, equalityCheck, keySelector }: MapProp
     </>
   )
 }
-
-type ChildProps<T> = {
+type MapWithKeyChildProps<T> = {
   arrayFacet: Facet<T[]>
   item: T
   index: number
@@ -43,7 +120,7 @@ type ChildProps<T> = {
   equalityCheck?: EqualityCheck<T>
 }
 
-const Child = <T,>({ arrayFacet, children, index, equalityCheck }: ChildProps<T>) => {
+const MapWithKeyChild = <T,>({ arrayFacet, children, index, equalityCheck }: MapWithKeyChildProps<T>) => {
   const childFacet = useFacetMemo(
     (array) => {
       if (index < array.length) return array[index]
