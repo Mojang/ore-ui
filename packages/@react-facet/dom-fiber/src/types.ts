74e63e984cf4fb6df6967964663445d95e4105f3
Fernando Via Canel <fernando.via@gmail.com>
2022-06-22 14:59:18 +0200

    Add SVG support (#63)
    * Add path and d support
    
    * Add fill prop support
    
    * Support stroke attribute
    
    * Add SVG element and width / heigh support
    
    * Support rect element and x / y attributes
    
    * Support circle element and cx / cy / r attributes
    
    * Add ellipse element and rx, ry attribute support
    
    * Add line element and x1, x2, y1, y2, strokeWidth attributes
    
    * added support for referencing graphics with <use>
    
    * Added support for xlink:href attribute
    
    * Added remaining elements and attributes from the list
    
    * Added generic set/remove attribute
    
    * 0.3.1-beta.0
    
    * Improve support for SVG, use the right element constructor
    
    * 0.3.1-beta.1
    
    * Undo beta versions to prepare for merging into main
    
    * Update yarn lock
    
    Co-authored-by: Viktor Bergehall <viktor@viktorbergehall.me>

--- packages/@react-facet/dom-fiber/src/types.ts
+++ packages/@react-facet/dom-fiber/src/types.ts
@@ -4,7 +4,7 @@ import { Key, MutableRefObject, ReactText } from 'react'
 
 export type FacetFiberRoot = FiberRoot
 
-export type Type =
+export type TypeHTML =
   | 'fast-a'
   | 'fast-div'
   | 'fast-span'
@@ -20,6 +20,41 @@ export type Type =
   | 'textarea'
   | 'input'
   | 'style'
+
+export type TypeSVG =
+  | 'fast-circle'
+  | 'fast-ellipse'
+  | 'fast-line'
+  | 'fast-path'
+  | 'fast-rect'
+  | 'fast-svg'
+  | 'fast-use'
+  | 'fast-polyline'
+  | 'fast-polygon'
+  | 'fast-linearGradient'
+  | 'fast-radialGradient'
+  | 'fast-stop'
+  | 'fast-svg-text'
+  | 'fast-pattern'
+  | 'circle'
+  | 'ellipse'
+  | 'line'
+  | 'path'
+  | 'rect'
+  | 'svg'
+  | 'defs'
+  | 'g'
+  | 'symbol'
+  | 'use'
+  | 'polyline'
+  | 'polygon'
+  | 'linearGradient'
+  | 'radialGradient'
+  | 'stop'
+  | 'text'
+  | 'pattern'
+
+export type Type = TypeHTML | TypeSVG
 
 export type InputType = 'text' | 'button' | 'password' | 'checkbox' | 'radio' | 'number'
 
@@ -92,20 +127,49 @@ export type ElementProps<T> = PointerEvents &
     dangerouslySetInnerHTML?: { __html: string }
 
     className?: FacetProp<string | undefined>
+    cx?: FacetProp<string | undefined>
+    cy?: FacetProp<string | undefined>
+    d?: FacetProp<string | undefined>
     ['data-droppable']?: FacetProp<boolean | undefined>
     ['data-testid']?: FacetProp<string | undefined>
     ['data-x-ray']?: FacetProp<boolean | undefined>
+    fill?: FacetProp<string | undefined>
     id?: FacetProp<string | undefined>
     src?: FacetProp<string | undefined>
+    height?: FacetProp<string | undefined>
     href?: FacetProp<string | undefined>
     target?: FacetProp<string | undefined>
     autoPlay?: FacetProp<boolean | undefined>
     loop?: FacetProp<boolean | undefined>
     disabled?: FacetProp<boolean | undefined>
     maxLength?: FacetProp<number | undefined>
+    r?: FacetProp<string | undefined>
+    rx?: FacetProp<string | undefined>
+    ry?: FacetProp<string | undefined>
     rows?: FacetProp<number | undefined>
-    value?: FacetProp<string | undefined>
+    stroke?: FacetProp<string | undefined>
+    strokeWidth?: FacetProp<string | undefined>
     type?: FacetProp<InputType | undefined>
+    value?: FacetProp<string | undefined>
+    x?: FacetProp<string | undefined>
+    x1?: FacetProp<string | undefined>
+    x2?: FacetProp<string | undefined>
+    width?: FacetProp<string | undefined>
+    y?: FacetProp<string | undefined>
+    y1?: FacetProp<string | undefined>
+    y2?: FacetProp<string | undefined>
+    viewBox?: FacetProp<string | undefined>
+    xLinkHref?: FacetProp<string | undefined>
+    fillOpacity?: FacetProp<string | undefined>
+    strokeOpacity?: FacetProp<string | undefined>
+    strokeLinecap?: FacetProp<string | undefined>
+    strokeLinejoin?: FacetProp<string | undefined>
+    points?: FacetProp<string | undefined>
+    offset?: FacetProp<string | undefined>
+    stopColor?: FacetProp<string | undefined>
+    stopOpacity?: FacetProp<string | undefined>
+    fontFamily?: FacetProp<string | undefined>
+    fontSize?: FacetProp<string | undefined>
   }
 
 export type TextProps = {
@@ -119,27 +183,56 @@ export type ValidPropsNames = keyof Props<unknown>
 export type ElementContainer = {
   children: Set<ElementContainer>
 
-  element: HTMLElement | Text
+  element: HTMLElement | SVGElement | Text
 
   style?: CSSStyleDeclaration
   styleUnsubscribers?: Map<string | number, Unsubscribe>
 
   className?: Unsubscribe
+  cx?: Unsubscribe
+  cy?: Unsubscribe
+  d?: Unsubscribe
   ['data-droppable']?: Unsubscribe
   ['data-testid']?: Unsubscribe
   ['data-x-ray']?: Unsubscribe
+  fill?: Unsubscribe
   id?: Unsubscribe
   src?: Unsubscribe
+  height?: Unsubscribe
   href?: Unsubscribe
   target?: Unsubscribe
   autoPlay?: Unsubscribe
   loop?: Unsubscribe
   disabled?: Unsubscribe
   maxLength?: Unsubscribe
+  r?: Unsubscribe
+  rx?: Unsubscribe
+  ry?: Unsubscribe
   rows?: Unsubscribe
-  value?: Unsubscribe
+  stroke?: Unsubscribe
+  strokeWidth?: Unsubscribe
   type?: Unsubscribe
   text?: Unsubscribe
+  value?: Unsubscribe
+  x?: Unsubscribe
+  x1?: Unsubscribe
+  x2?: Unsubscribe
+  width?: Unsubscribe
+  y?: Unsubscribe
+  y1?: Unsubscribe
+  y2?: Unsubscribe
+  viewBox?: Unsubscribe
+  xLinkHref?: Unsubscribe
+  fillOpacity?: Unsubscribe
+  strokeOpacity?: Unsubscribe
+  strokeLinecap?: Unsubscribe
+  strokeLinejoin?: Unsubscribe
+  points?: Unsubscribe
+  offset?: Unsubscribe
+  stopColor?: Unsubscribe
+  stopOpacity?: Unsubscribe
+  fontFamily?: Unsubscribe
+  fontSize?: Unsubscribe
 }
 
 export const isElementContainer = (value: ElementContainer | TextContainer): value is ElementContainer => {
@@ -154,7 +247,7 @@ export type Instance = ElementContainer
 export type Container = Instance
 export type TextInstance = TextContainer
 export type HydratableInstance = Instance
-export type PublicInstance = HTMLElement | Text
+export type PublicInstance = HTMLElement | SVGElement | Text
 
 export type ReactFacetReconciler = Reconciler<Instance, TextInstance, Container, PublicInstance> & {
   flushPassiveEffects: () => boolean
@@ -168,13 +261,27 @@ export interface HostContext {
 export type UpdatePayload = boolean
 
 export type FastAProps = ElementProps<HTMLAnchorElement>
+export type FastCircleProps = ElementProps<SVGCircleElement>
 export type FastDivProps = ElementProps<HTMLDivElement>
+export type FastEllipseProps = ElementProps<SVGEllipseElement>
+export type FastLineProps = ElementProps<SVGLineElement>
 export type FastImgProps = ElementProps<HTMLImageElement>
 export type FastTextareaProps = ElementProps<HTMLTextAreaElement>
 export type FastInputProps = ElementProps<HTMLInputElement>
 export type FastPProps = ElementProps<HTMLParagraphElement>
+export type FastPathProps = ElementProps<SVGPathElement>
+export type FastRectProps = ElementProps<SVGRectElement>
 export type FastSpanProps = ElementProps<HTMLSpanElement>
+export type FastSvgProps = ElementProps<SVGSVGElement>
 export type FastTextProps = TextProps
+export type FastUseProps = ElementProps<SVGUseElement>
+export type FastPolylineProps = ElementProps<SVGPolylineElement>
+export type FastPolyGonProps = ElementProps<SVGPolygonElement>
+export type FastLinearGradientProps = ElementProps<SVGLinearGradientElement>
+export type FastRadialGradientProps = ElementProps<SVGRadialGradientElement>
+export type FastStopProps = ElementProps<SVGStopElement>
+export type FastSvgTextProps = ElementProps<SVGTextElement>
+export type FastPatternProps = ElementProps<SVGPatternElement>
 
 /**
  * Extends React global namespace with the "fast" types
@@ -184,13 +291,27 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       'fast-a': FastAProps
+      'fast-circle': FastCircleProps
+      'fast-ellipse': FastEllipseProps
+      'fast-line': FastLineProps
       'fast-div': FastDivProps
       'fast-img': FastImgProps
       'fast-textarea': FastTextareaProps
       'fast-input': FastInputProps
       'fast-p': FastPProps
+      'fast-path': FastPathProps
+      'fast-rect': FastRectProps
       'fast-span': FastSpanProps
       'fast-text': FastTextProps
+      'fast-svg': FastSvgProps
+      'fast-use': FastUseProps
+      'fast-polyline': FastPolylineProps
+      'fast-polygon': FastPolyGonProps
+      'fast-linearGradient': FastLinearGradientProps
+      'fast-radialGradient': FastRadialGradientProps
+      'fast-stop': FastStopProps
+      'fast-svg-text': FastSvgTextProps
+      'fast-pattern': FastPatternProps
     }
   }
 }
