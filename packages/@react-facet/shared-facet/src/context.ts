0a8e7c47bd5566b4c8c75b23703fc6004918c9d2
Fernando Via Canel <fernando.via@gmail.com>
2022-07-06 13:41:27 +0200

    Remove lint

--- packages/@react-facet/shared-facet/src/context.ts
+++ packages/@react-facet/shared-facet/src/context.ts
@@ -1,13 +1,8 @@
-import { Facet } from '@react-facet/core'
-import { createContext, useContext } from 'react'
-import { SharedFacet, SharedFacetDriver } from './types'
+import { createContext } from 'react'
+import { SharedFacetDriver } from './types'
 
 const dummyConstructor = () => () => {}
 
 export const sharedFacetDriverContext = createContext<SharedFacetDriver>(dummyConstructor)
 
 export const SharedFacetDriverProvider = sharedFacetDriverContext.Provider
-
-export const useSharedFacet = <T>(sharedFacet: SharedFacet<T>): Facet<T> => {
-  return sharedFacet(useContext(sharedFacetDriverContext))
-}
