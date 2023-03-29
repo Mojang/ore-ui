613383aaab93a16db08ab3ed3847c701c6845423
AdamRamberg <adam@mambojambostudios.com>
2023-03-29 09:35:26 +0200

    Update the item facet, which was stale before this commit

--- packages/@react-facet/core/src/components/MapWithKey.tsx
+++ packages/@react-facet/core/src/components/MapWithKey.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useRef, useState, memo, useMemo } from 'react'
 import { createFacet } from '../facet/createFacet'
 import { useFacetEffect } from '../hooks/useFacetEffect'
-import { EqualityCheck, Facet, NO_VALUE } from '../types'
+import { EqualityCheck, Facet, NO_VALUE, WritableFacet } from '../types'
 
 export type MapWithKeysProps<T> = {
   array: Facet<T[]>
@@ -12,6 +12,7 @@ export type MapWithKeysProps<T> = {
 
 export const MapWithKey = <T,>({ array, children, keySelector, equalityCheck }: MapWithKeysProps<T>) => {
   const currentKeys = useRef<React.Key[]>([])
+  const itemFacetsMap = useRef<Map<React.Key, WritableFacet<T>>>(new Map())
   const [, rerender] = useState(0)
 
   useFacetEffect(
@@ -19,15 +20,20 @@ export const MapWithKey = <T,>({ array, children, keySelector, equalityCheck }: 
       let shouldRerender = array.length != currentKeys.current.length
       const newKeys = []
 
-      // Even though shouldRerender is true, we still need to loop through every item in the array
-      // in order to make sure no keys have changed and update previousKeys accordingly.
       for (let i = 0; i < array.length; i++) {
+        const item = array[i]
+        // Get key
         const key = keySelector != null ? keySelector(array[i]) : i
         newKeys.push(key)
 
-        if (!shouldRerender && newKeys[i] !== currentKeys.current[i]) {
-          shouldRerender = true
+        // Update the item facet
+        const itemFacet = itemFacetsMap.current.get(key)
+        if (itemFacet != null) {
+          itemFacet.set(item)
         }
+
+        // Determine if we need to rerender
+        shouldRerender = shouldRerender || key !== currentKeys.current[i]
       }
 
       if (shouldRerender) {
@@ -35,7 +41,7 @@ export const MapWithKey = <T,>({ array, children, keySelector, equalityCheck }: 
         rerender((value) => value + 1)
       }
     },
-    [keySelector],
+    [keySelector, equalityCheck],
     [array],
   )
 
@@ -53,6 +59,7 @@ export const MapWithKey = <T,>({ array, children, keySelector, equalityCheck }: 
             children={children as (item: Facet<unknown>, index: number) => ReactElement | null}
             item={item}
             equalityCheck={equalityCheck as EqualityCheck<unknown> | undefined}
+            itemFacetsMap={itemFacetsMap}
           />
         )
       })}
@@ -66,11 +73,18 @@ type MapChildProps<T> = {
   children: (item: Facet<T>, index: number) => ReactElement | null
   equalityCheck?: EqualityCheck<T>
   memoKey: React.Key
+  itemFacetsMap: React.MutableRefObject<Map<React.Key, Facet<T>>>
 }
 
-const MapChild = <T,>({ item, children, index, equalityCheck }: MapChildProps<T>) => {
+const MapChild = <T,>({ item, children, index, equalityCheck, itemFacetsMap, memoKey }: MapChildProps<T>) => {
   const itemFacet = useMemo(
-    () => createFacet<T>({ initialValue: item, equalityCheck }),
+    () => {
+      // Create the facet
+      const facet = createFacet<T>({ initialValue: item, equalityCheck })
+      // Update the facet map
+      itemFacetsMap.current.set(memoKey, facet)
+      return facet
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   )
