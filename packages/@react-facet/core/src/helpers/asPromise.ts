6057d3a35c6fb1166743644135c9d0592f27b7d9
Paulo Ragonha <paulo@ragonha.me>
2023-06-05 14:56:18 +0200

    Make sure asPromise works when there are values immediately

--- packages/@react-facet/core/src/helpers/asPromise.ts
+++ packages/@react-facet/core/src/helpers/asPromise.ts
@@ -1,4 +1,4 @@
-import { Facet } from '../types'
+import { Facet, NO_VALUE } from '../types'
 
 /**
  * Takes a Facet as returns a Promise that will resolve with its first value
@@ -8,6 +8,12 @@ import { Facet } from '../types'
  */
 export const asPromise = <T>(facet: Facet<T>) =>
   new Promise<T>((resolve) => {
+    const value = facet.get()
+    if (value != NO_VALUE) {
+      resolve(value)
+      return
+    }
+
     const cleanup = facet.observe((value: T) => {
       resolve(value)
       cleanup()
