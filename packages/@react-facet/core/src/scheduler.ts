b6086defd7f83fbadc9a73945804d9dabaaf48f3
Paulo Ragonha <paulo@ragonha.me>
2023-06-11 20:12:36 +0200

    Cleanup development log, plus some extra docs

--- packages/@react-facet/core/src/scheduler.ts
+++ packages/@react-facet/core/src/scheduler.ts
@@ -2,30 +2,29 @@ import { Task, Batch } from './types'
 
 let batchId = 0
 let taskQueue: Task[] = []
-const taskCounter = new Map<Task, number>()
 
 /**
  * Schedules a given task to be executed at the end of a current batch, or runs it immediately if no batch is active.
  * @param task
- * @returns
  */
 export const scheduleTask = (task: Task) => {
+  // Not currently within a batch, so we execute the task immediately.
   if (batchId === 0) {
     task()
     return
   }
 
-  if (process.env.NODE_ENV === 'development') {
-    const currentCount = taskCounter.get(task) ?? 0
-    taskCounter.set(task, currentCount + 1)
-  }
-
+  // Only schedules a task once within this batch execution.
   if (!task.scheduled) {
     task.scheduled = true
     taskQueue.push(task)
   }
 }
 
+/**
+ * Cancels the scheduling of a previously scheduled task.
+ * @param task
+ */
 export const cancelScheduledTask = (task: Task) => {
   // Mark a task as canceled instead of removing it.
   // Its reference might already have been taken while processing the tasks.
@@ -44,18 +43,8 @@ export const batch = (b: Batch) => {
 
   // If this is the root batch, we start executing the tasks
   if (batchId === 1) {
-    if (process.env.NODE_ENV === 'development') {
-      const taskCounterCopy = Array.from(taskCounter)
-      taskCounter.clear()
-
-      const optimizedCount = taskCounterCopy.filter((pair) => pair[1] > 1).length
-      if (taskCounterCopy.length > 0) {
-        console.log(`⚒️ Total: ${taskCounterCopy.length}. Optimized: ${optimizedCount}`)
-      }
-    }
-
     do {
-      // Make a copy of the schedule, as running a task can cause other tasks to be scheduled
+      // Starts a new queue, as we work through the current one
       const taskQueueCopy = taskQueue
       taskQueue = []
 
