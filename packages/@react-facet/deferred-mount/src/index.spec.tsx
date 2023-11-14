f505c56206a19c6c49b579957a6a5b1f6699b12f
Paulo <paulo@ragonha.me>
2023-11-14 16:16:45 +0100

    Ops, fixing TypeScript issue

--- packages/@react-facet/deferred-mount/src/index.spec.tsx
+++ packages/@react-facet/deferred-mount/src/index.spec.tsx
@@ -1,1 +1,5 @@
 it.todo('test deprecated implementation of deferred mount using useTransition')
+
+// Make TypeScript happy --isolatedModules
+// Can be removed once tests are written, and are imports on this file
+export default {}
