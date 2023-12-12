c98cc2a633f1dd85ee8f8ec8117afa602fbccf8a
Paulo <paulo@ragonha.me>
2023-12-12 16:19:39 +0100

    Small refactor to early bail

--- packages/@react-facet/core/src/hooks/useFacetUnwrap.ts
+++ packages/@react-facet/core/src/hooks/useFacetUnwrap.ts
@@ -21,53 +21,53 @@ export function useFacetUnwrap<T extends Value>(
   })
 
   useLayoutEffect(() => {
-    if (isFacet(prop)) {
-      // Initialize the equalityCheck
-      const isEqual = equalityCheck()
-      const startValue = prop.get()
-      if (startValue !== NO_VALUE) {
-        isEqual(startValue)
-      }
+    if (!isFacet(prop)) return
 
-      return prop.observe((value) => {
-        setState((previousState) => {
-          const { value: previousValue } = previousState
+    // Initialize the equalityCheck
+    const isEqual = equalityCheck()
+    const startValue = prop.get()
+    if (startValue !== NO_VALUE) {
+      isEqual(startValue)
+    }
 
-          /**
-           * Performs this equality check locally to prevent triggering two consecutive renderings
-           * for facets that have immutable values (unfortunately we can't handle mutable values).
-           *
-           * The two renderings might happen for the same state value if the Facet has a value on mount.
-           *
-           * The unwrap will get the value:
-           * - Once on initialization of the useState above
-           * - And another time on this observe initialization
-           */
-          if (equalityCheck === defaultEqualityCheck) {
-            const typeofValue = typeof previousValue
+    return prop.observe((value) => {
+      setState((previousState) => {
+        const { value: previousValue } = previousState
 
-            if (
-              (typeofValue === 'number' ||
-                typeofValue === 'string' ||
-                typeofValue === 'boolean' ||
-                value === undefined ||
-                value === null) &&
-              value === previousValue
-            ) {
-              return previousState
-            }
+        /**
+         * Performs this equality check locally to prevent triggering two consecutive renderings
+         * for facets that have immutable values (unfortunately we can't handle mutable values).
+         *
+         * The two renderings might happen for the same state value if the Facet has a value on mount.
+         *
+         * The unwrap will get the value:
+         * - Once on initialization of the useState above
+         * - And another time on this observe initialization
+         */
+        if (equalityCheck === defaultEqualityCheck) {
+          const typeofValue = typeof previousValue
 
-            return { value }
-          }
-
-          if (previousValue !== NO_VALUE && isEqual(previousValue)) {
+          if (
+            (typeofValue === 'number' ||
+              typeofValue === 'string' ||
+              typeofValue === 'boolean' ||
+              value === undefined ||
+              value === null) &&
+            value === previousValue
+          ) {
             return previousState
           }
 
           return { value }
-        })
+        }
+
+        if (previousValue !== NO_VALUE && isEqual(previousValue)) {
+          return previousState
+        }
+
+        return { value }
       })
-    }
+    })
   }, [prop, equalityCheck])
 
   return isFacet(prop) ? state.value : prop
