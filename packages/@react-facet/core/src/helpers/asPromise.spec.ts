e22975dfad87b51110a3d4847bb988292e9d6313
Paulo Ragonha <paulo@ragonha.me>
2024-04-10 12:41:39 -0300

    Fix asPromise race condition

--- packages/@react-facet/core/src/helpers/asPromise.spec.ts
+++ packages/@react-facet/core/src/helpers/asPromise.spec.ts
@@ -1,10 +1,28 @@
+import { NO_VALUE } from '..'
 import { createFacet } from '../facet'
 import { asPromise } from './asPromise'
 
-it('resolves facets with their values if available', async () => {
+it('immediately resolves facets with their values if available', async () => {
   const facet = createFacet({ initialValue: 'testing' })
-  const value = await asPromise(facet)
+  const value = await asPromise(facet).promise
   expect(value).toEqual('testing')
 })
 
-it.todo('waits for the facet to have a value, making sure to not hold subscriptions')
+it('waits for the facet to have a value, making sure to not hold subscriptions', async () => {
+  const cleanupSubscription = jest.fn()
+  const startSubscription = jest.fn().mockImplementation((update) => {
+    update('testing')
+    return cleanupSubscription
+  })
+
+  const facet = createFacet<string>({
+    initialValue: NO_VALUE,
+    startSubscription,
+  })
+
+  const value = await asPromise(facet).promise
+  expect(value).toEqual('testing')
+
+  expect(startSubscription).toHaveBeenCalledTimes(1)
+  expect(cleanupSubscription).toHaveBeenCalledTimes(1)
+})
--- packages/@react-facet/core/src/helpers/asPromise.ts
+++ packages/@react-facet/core/src/helpers/asPromise.ts
@@ -1,21 +1,37 @@
 import { Facet, NO_VALUE } from '../types'
 
 /**
- * Takes a Facet as returns a Promise that will resolve with its first value
+ * Takes a Facet and returns a Promise that will resolve with its first value
  *
  * @param facet
  * @returns Promise<T>
  */
-export const asPromise = <T>(facet: Facet<T>) =>
-  new Promise<T>((resolve) => {
-    const value = facet.get()
-    if (value !== NO_VALUE) {
-      resolve(value)
-      return
-    }
+export const asPromise = <T>(facet: Facet<T>) => {
+  let resolve: (value: T | PromiseLike<T>) => void = noop
 
-    const cleanup = facet.observe((value: T) => {
-      resolve(value)
-      cleanup()
-    })
+  const promise = new Promise<T>((_resolve) => {
+    resolve = _resolve
   })
+
+  const value = facet.get()
+  if (value !== NO_VALUE) {
+    resolve(value)
+
+    return {
+      promise,
+      cancel: noop,
+    }
+  }
+
+  const cleanup = facet.observe(resolve)
+
+  return {
+    promise: promise.then((value) => {
+      cleanup()
+      return value
+    }),
+    cancel: cleanup,
+  }
+}
+
+const noop = () => {}
