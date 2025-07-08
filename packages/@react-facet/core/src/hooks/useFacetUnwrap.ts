acf37ae1f1b1da1adacf6df9728f4933d98ec807
Omar ElGaml <omarelgaml@mojang.com>
2025-07-08 16:32:12 +0200

    Re-worked useFacetUnwrap to initialize the facet before creating the state, thereby having a value on initial render instead of NO_VALUE.

--- packages/@react-facet/core/src/hooks/useFacetUnwrap.ts
+++ packages/@react-facet/core/src/hooks/useFacetUnwrap.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useLayoutEffect, useMemo, useState } from 'react'
 import { FacetProp, isFacet, Value, NoValue, EqualityCheck, NO_VALUE } from '../types'
 import { defaultEqualityCheck } from '../equalityChecks'
 
@@ -12,6 +12,11 @@ export function useFacetUnwrap<T extends Value>(
   prop: FacetProp<T>,
   equalityCheck: EqualityCheck<T> = defaultEqualityCheck,
 ): T | NoValue {
+  const initializationCleanup = useMemo(() => {
+    if (!isFacet(prop)) return () => {}
+    return prop.observe(() => {})
+  }, [prop])
+
   const [state, setState] = useState<{ value: T | NoValue }>(() => {
     if (!isFacet(prop)) return { value: prop }
 
@@ -20,55 +25,59 @@ export function useFacetUnwrap<T extends Value>(
     }
   })
 
-  useEffect(() => {
-    if (!isFacet(prop)) return
+  useLayoutEffect(() => {
+    if (isFacet(prop)) {
+      // Initialize the equalityCheck
+      const isEqual = equalityCheck()
+      const startValue = prop.get()
+      if (startValue !== NO_VALUE) {
+        isEqual(startValue)
+      }
 
-    // Initialize the equalityCheck
-    const isEqual = equalityCheck()
-    const startValue = prop.get()
-    if (startValue !== NO_VALUE) {
-      isEqual(startValue)
-    }
+      const cleanup = prop.observe((value) => {
+        setState((previousState) => {
+          const { value: previousValue } = previousState
 
-    return prop.observe((value) => {
-      setState((previousState) => {
-        const { value: previousValue } = previousState
+          /**
+           * Performs this equality check locally to prevent triggering two consecutive renderings
+           * for facets that have immutable values (unfortunately we can't handle mutable values).
+           *
+           * The two renderings might happen for the same state value if the Facet has a value on mount.
+           *
+           * The unwrap will get the value:
+           * - Once on initialization of the useState above
+           * - And another time on this observe initialization
+           */
+          if (equalityCheck === defaultEqualityCheck) {
+            const typeofValue = typeof previousValue
 
-        /**
-         * Performs this equality check locally to prevent triggering two consecutive renderings
-         * for facets that have immutable values (unfortunately we can't handle mutable values).
-         *
-         * The two renderings might happen for the same state value if the Facet has a value on mount.
-         *
-         * The unwrap will get the value:
-         * - Once on initialization of the useState above
-         * - And another time on this observe initialization
-         */
-        if (equalityCheck === defaultEqualityCheck) {
-          const typeofValue = typeof previousValue
+            if (
+              (typeofValue === 'number' ||
+                typeofValue === 'string' ||
+                typeofValue === 'boolean' ||
+                value === undefined ||
+                value === null) &&
+              value === previousValue
+            ) {
+              return previousState
+            }
 
-          if (
-            (typeofValue === 'number' ||
-              typeofValue === 'string' ||
-              typeofValue === 'boolean' ||
-              value === undefined ||
-              value === null) &&
-            value === previousValue
-          ) {
+            return { value }
+          }
+
+          if (previousValue !== NO_VALUE && isEqual(value)) {
             return previousState
           }
 
           return { value }
-        }
-
-        if (previousValue !== NO_VALUE && isEqual(value)) {
-          return previousState
-        }
-
-        return { value }
+        })
       })
-    })
-  }, [prop, equalityCheck])
+
+      initializationCleanup()
+
+      return cleanup
+    }
+  }, [prop, equalityCheck, initializationCleanup])
 
   return isFacet(prop) ? state.value : prop
 }
