4e77a98fdede3b0041893463f12ea0dec26b3f4a
Paulo Ragonha <paulo@ragonha.me>
2025-09-17 09:45:41 +0200

    Fix types of useFacetTransition

--- packages/@react-facet/core/src/hooks/useFacetTransition.tsx
+++ packages/@react-facet/core/src/hooks/useFacetTransition.tsx
@@ -2,7 +2,7 @@ import { useTransition, useCallback } from 'react'
 import { Batch } from '../types'
 import { batchTransition } from '../scheduler'
 
-export const useFacetTransition = () => {
+export const useFacetTransition = (): [boolean, (fn: () => void) => void] => {
   const [isPending, reactStartTransition] = useTransition()
 
   return [
