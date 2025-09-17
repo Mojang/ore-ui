a3f5aabb19b4bab37591910faaad764c1845ec71
Paulo Ragonha <paulo@ragonha.me>
2025-09-17 10:00:59 +0200

    Documentation

--- packages/@react-facet/core/src/hooks/useFacetTransition.tsx
+++ packages/@react-facet/core/src/hooks/useFacetTransition.tsx
@@ -2,6 +2,10 @@ import { useTransition, useCallback } from 'react'
 import { Batch } from '../types'
 import { batchTransition } from '../scheduler'
 
+/**
+ * Hook that acts analogous to React's useTransition, ensuring that any React state change that happens as a
+ * result of a Facet update is handled within a React transition.
+ */
 export const useFacetTransition = (): [boolean, (fn: () => void) => void] => {
   const [isPending, reactStartTransition] = useTransition()
 
